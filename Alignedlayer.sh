e2a1ba3dfcf3c624a0abe91104252c98d0f3101b
smeb y <48400087+a3165458@users.noreply.github.com>
2024-04-23 15:13:04 +0800

    Update Alignedlayer.sh

--- Alignedlayer.sh
+++ Alignedlayer.sh
@@ -209,7 +209,7 @@ function uninstall_node() {
 function delegate_self_validator() {
 read -p "请输入质押代币数量: " math
 read -p "请输入钱包名称: " wallet_name
-alignedlayerd tx staking delegate $(alignedlayerd keys show wallet --bech val -a)  ${math}stake --from $wallet_name --chain-id alignedlayer --fees 50stake
+alignedlayerd tx staking delegate $(alignedlayerd keys show wallet --bech val -a)  ${math}stake --from $wallet_name --chain-id alignedlayer --fees 50stake --node $node_address
 
 }
 
